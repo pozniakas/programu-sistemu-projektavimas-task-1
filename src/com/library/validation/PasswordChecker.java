96d5e577419f9e3e9dc993233851d109bc88aadf
Domas Pozniakas <domaspoz@gmail.com>
2021-09-15 01:16:39 +0300

    Refactoring of TODO message in isValidPassword method

--- src/com/library/validation/PasswordChecker.java
+++ src/com/library/validation/PasswordChecker.java
@@ -4,9 +4,9 @@ public class PasswordChecker {
 	public boolean isValidPassowrd(String password) {
 		/*
 		 * TODO: Implement logic which will validate provided password by these
-		 * criteria: checks if the password is at least X long, checks for uppercase
-		 * characters, checks for a special character (list of special characters must
-		 * be configured)
+		 * criteria: checks if the password is at least 5 symbols long, checks for
+		 * uppercase characters, checks for a special character (list of special
+		 * characters must be configured)
 		 */
 		throw new UnsupportedOperationException("Not implemented");
 	}
