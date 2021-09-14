ded733768ea8b352f4427d31e33bf13d0f4b765f
Domas Pozniakas <domaspoz@gmail.com>
2021-09-14 23:05:09 +0300

    Createdd validation classes by business requirements without implemented logic

---
+++ src/com/library/validation/EmailValidator.java
@@ -0,0 +1,12 @@
+package com.library.validation;
+
+public class EmailValidator {
+    public boolean isEmailValid(String email) {
+    	/* 
+    	 * TODO: Implement logic which will validate provided email by these criteria:
+    	 * 1. Checks if the email has @ symbol
+    	 * 2. Checks if the email has no invalid characters
+    	 */
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
---
+++ src/com/library/validation/PasswordChecker.java
@@ -0,0 +1,13 @@
+package com.library.validation;
+
+public class PasswordChecker {
+    public boolean isValidPassowrd(String password) {
+    	/* 
+    	 * TODO: Implement logic which will validate provided password by these criteria:
+    	 * 1. Checks if the password is at least X long
+    	 * 2. Checks for uppercase characters
+    	 * 3. Checks for a special character (list of special characters must be configured)
+    	 */
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
