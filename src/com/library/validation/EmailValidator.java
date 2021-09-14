7705a7ffc215127b263d94c0e08fb2d2906ff9f6
Domas Pozniakas <domaspoz@gmail.com>
2021-09-15 01:03:21 +0300

    Auto-refactoring by eclipse autoformatter in src folder

--- src/com/library/validation/EmailValidator.java
+++ src/com/library/validation/EmailValidator.java
@@ -1,13 +1,12 @@
 package com.library.validation;
 
 public class EmailValidator {
-    public boolean isValidEmail(String email) {
-    	/* 
-    	 * TODO: Implement logic which will validate provided email by these criteria:
-    	 * 1. Checks if the email has @ symbol
-    	 * 2. Checks if the email has no invalid characters
-    	 * 3. Checks if the email has the correct domain and TLD
-    	 */
-        throw new UnsupportedOperationException("Not implemented");
-    }
+	public boolean isValidEmail(String email) {
+		/*
+		 * TODO: Implement logic which will validate provided email by these criteria:
+		 * checks if the email has @ symbol, has no invalid , has the correct domain and
+		 * TLD
+		 */
+		throw new UnsupportedOperationException("Not implemented");
+	}
 }
