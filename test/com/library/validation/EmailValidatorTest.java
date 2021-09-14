2da2db0c8fb891a263126b1674f04e79b04812c9
Domas Pozniakas <domaspoz@gmail.com>
2021-09-15 00:45:47 +0300

    Added unit tests for EmailValidator class

---
+++ test/com/library/validation/EmailValidatorTest.java
@@ -0,0 +1,49 @@
+package com.library.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class EmailValidatorTest {
+    private final EmailValidator emailValidator = new EmailValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"moon@gmail.com", "earth@yahoo.com", "steve@inbox.lt", "support@pozniakas.com"})
+    void testValidEmailValidation(String email) {
+        assertTrue(emailValidator.isValidEmail(email));
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"moo_n@gmail.com", "e;arth@yahoo.com", "steve@/na.lt", "support @pozniakas.com"})
+    void testEmailWithIlleagalSymbolsValidation(String email) {
+        assertFalse(emailValidator.isValidEmail(email));
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "    "})
+    void testPhoneValidationWithEmptyValue(String email) {
+        assertFalse(emailValidator.isValidEmail(email));
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"moongmail.com", "yahoo.com", "steve.lt", "pozniakas"})
+    void testEmailWithoutAtSignValidation(String email) {
+        assertFalse(emailValidator.isValidEmail(email));
+    }
+ 
+    @Test
+    void testEmailWithInvalidDomainValidation() {
+    	String email = "earth@neegzistuojanti-svetaine.net";
+
+        assertFalse(emailValidator.isValidEmail(email));
+    }
+    
+    @Test
+    void testEmailWithInvalidTLDValidation() {
+    	String email = "earth@gmail.blogastld";
+
+        assertFalse(emailValidator.isValidEmail(email));
+    }
+}
