579b2856606a7f273880acb01f9694b252227d4d
Domas Pozniakas <domaspoz@gmail.com>
2021-10-02 21:43:59 +0300

    Added PhoneValidator and PasswordChecker business logic

--- src/com/library/validation/PasswordChecker.java
+++ src/com/library/validation/PasswordChecker.java
@@ -1,13 +1,41 @@
 package com.library.validation;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class PasswordChecker {
+	private static int MIN_PASSWORD_LENGTH = 5;
+	private static List<Character> DEFAULT_SPECIAL_CHARACTERS = Arrays.asList('!', '@', '#', '$', '%', '^', '&', '*',
+			'(', ')', '<', '>');
+
+	private List<Character> specialCharacters;
+
+	public PasswordChecker() {
+		this.specialCharacters = DEFAULT_SPECIAL_CHARACTERS;
+	}
+
+	public PasswordChecker(List<Character> specialCharacters) {
+		this.specialCharacters = specialCharacters;
+	}
+
 	public boolean validate(String password) {
-		/*
-		 * TODO: Implement logic which will validate provided password by these
-		 * criteria: checks if the password is at least 5 symbols long, checks for
-		 * uppercase characters, checks for a special character (list of special
-		 * characters must be configured)
-		 */
-		throw new UnsupportedOperationException("Not implemented");
+		return password != null && this.isValidLength(password) && this.hasSpecialCharacter(password)
+				&& this.hasUppercaseCharacter(password);
+	}
+
+	private boolean isValidLength(String password) {
+		return password.length() >= MIN_PASSWORD_LENGTH;
+	}
+
+	private boolean hasSpecialCharacter(String password) {
+		Stream<Character> specialCharactersStream = specialCharacters.stream();
+		return specialCharactersStream.anyMatch(character -> password.contains(character.toString()));
+	}
+
+	private boolean hasUppercaseCharacter(String password) {
+		IntStream passwordInChars = password.chars();
+		return passwordInChars.anyMatch(Character::isUpperCase);
 	}
 }
--- src/com/library/validation/PhoneValidator.java
+++ src/com/library/validation/PhoneValidator.java
@@ -1,16 +1,56 @@
 package com.library.validation;
 
 public class PhoneValidator {
+	private static String DEFAULT_INTERNATIONAL_CALLING_CODE = "+370";
+	private static String DEFAULT_NATIONAL_CALLING_CODE = "8";
+	private static int DEFAULT_PHONE_NUMBER_LENGTH = 8;
+
+	private String internationalCallingCode;
+	private String nationalCallingCode;
+	private int phoneNumberLength;
+
+	public PhoneValidator() {
+		this.internationalCallingCode = DEFAULT_INTERNATIONAL_CALLING_CODE;
+		this.nationalCallingCode = DEFAULT_NATIONAL_CALLING_CODE;
+		this.phoneNumberLength = DEFAULT_PHONE_NUMBER_LENGTH;
+	}
+
+	public PhoneValidator(String internationalCallingCode, String nationalCallingCode, int phoneNumberLength) {
+		this.internationalCallingCode = internationalCallingCode;
+		this.nationalCallingCode = nationalCallingCode;
+		this.phoneNumberLength = phoneNumberLength;
+	}
+
 	public boolean validate(String phone) {
-		/*
-		 * TODO: Implement logic which will validate provided phone by criteria: there
-		 * are no other characters than numbers
-		 */
-		throw new UnsupportedOperationException("Not implemented");
+		if (phone == null || !isLegalPhonePrefix(phone)) {
+			return false;
+		}
+		phone = this.convert(phone);
+
+		return isValidPhoneLength(phone) && isPhoneWithoutIllegalCharacters(phone);
 	}
 
 	public String convert(String phone) {
-		// TODO: Add functionality to change prefix 8 to +370
-		throw new UnsupportedOperationException("Not implemented");
+		boolean isInvalidPrefix = !isLegalPhonePrefix(phone);
+		if (isInvalidPrefix) {
+			throw new IllegalArgumentException("Invalid phone number to convert prefix");
+		}
+
+		return phone.startsWith(internationalCallingCode) ? phone
+				: internationalCallingCode + phone.substring(nationalCallingCode.length());
+	}
+
+	private boolean isLegalPhonePrefix(String phone) {
+		return phone != null && (phone.startsWith(nationalCallingCode) || phone.startsWith(internationalCallingCode));
+	}
+
+	private boolean isValidPhoneLength(String phone) {
+		int callingCodeLength = internationalCallingCode.length();
+		return phone.length() >= phoneNumberLength ? phone.substring(callingCodeLength).length() == phoneNumberLength
+				: false;
+	}
+
+	private boolean isPhoneWithoutIllegalCharacters(String phone) {
+		return phone.substring(1).chars().allMatch(Character::isDigit);
 	}
 }
